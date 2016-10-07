58430f197d1af1a63d083bb5eb52c33c35fb7cf8
Beita31 <emhernandezb@gmail.com>
2016-10-06 18:21:52 -0600

    Update ContactoManagerImplTest.java
    Was not the updated one since last week.

--- proyecto/src/test/java/com/una/proyecto/webapp/service/impl/ContactoManagerImplTest.java
+++ proyecto/src/test/java/com/una/proyecto/webapp/service/impl/ContactoManagerImplTest.java
@@ -26,20 +26,22 @@ public class ContactoManagerImplTest extends BaseManagerMockTestCase {
 	@Test
 	public void testObtenerCampoJuego() {
 		// given
-		final Long id = 1L;
+		final Long id = new Long(115360687);
 		final Contacto contacto = new Contacto();
-		contacto.setNombre("Julio Barrera");
+		contacto.setNombre("Premier #5");
 
 		given(dao.get(id)).willReturn(contacto);
 
 		// when
-		CampoJuego campoJuego = manager.obtenerCampoJuego("Julio Barrera");
+		CampoJuego campoJuego = manager.obtenerCampoJuego("Premier #5");
 		
 		// then
 		assertTrue(campoJuego != null);
-		List<Contacto> contacto2 = dao.findByName("Julio Barrera");
-		if(contacto2.size() > 0){
-			assertEquals(contacto2.get(0).getCampoJuego(), campoJuego);
+		List<Contacto> contacto2 = dao.findByName("Premier #5");
+		if(contacto2!= null){
+			if(contacto2.size() > 0){
+				assertEquals(contacto2.get(0).getCampoJuego(), campoJuego);
+			}
 		}
 	}
 	
